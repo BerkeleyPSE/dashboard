4769138f4acfe812ef7c76c187f87429d6c6e7bd
Rahul Rangnekar <rrangnekar96@gmail.com>
2018-03-26 16:42:44 -0700

    add ScrollToTop component

--- client/src/index.js
+++ client/src/index.js
@@ -10,6 +10,7 @@ import reduxThunk from 'redux-thunk';
 // local components
 import './index.css';
 import App from './App';
+import ScrollToTop from './containers/ScrollToTop';
 import rootReducer from './reducers/index';
 import registerServiceWorker from './registerServiceWorker';
 
@@ -19,7 +20,9 @@ const store = createStore(rootReducer, {}, composeEnhancers(applyMiddleware(redu
 ReactDOM.render(
   <Provider store={store}>
     <Router>
-      <App />
+      <ScrollToTop>
+        <App />
+      </ScrollToTop>
     </Router>
   </Provider>,
   document.getElementById('root')
